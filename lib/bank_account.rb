4d3c52d0b49be32887ad6c15bf6f32aecf3b8e78
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-20 14:12:46 +0100

    bank_account.rb cleaned with rubocop

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,20 @@
+# frozen_string_literal: true
+
+# BankAccount class accpets deposits, withdrawals and check balance
 class BankAccount
   def initialize
     @deposit = false
   end
 
   def statement
-    return "date || credit || debit || balance\n"\
-    "20/09/2022 || 200.00 || || 200.00" if @deposit == true
-    "date || credit || debit || balance"
+    if @deposit == true
+      return 'date || credit || debit || balance\n'\
+      '20/09/2022 || 200.00 || || 200.00'
+    end
+    'date || credit || debit || balance'
   end
 
-  def deposit(amount)
+  def deposit(_amount)
     @deposit = true
   end
 end
