4180edfefb1c4f2db5f16d5edcff5d7aec5f4211
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-20 16:05:09 +0100

    refactor bank_account.rb

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,31 +1,28 @@
 # frozen_string_literal: true
 
-# BankAccount class accpets deposits, withdrawals and check balance
+# BankAccount class accepts deposits, withdrawals, and check statement
 class BankAccount
   def initialize
     @balance = 0
-    @deposit = 0
+    @transactions = []
   end
 
   def statement
-    if @balance == 190
-      return 'date || credit || debit || balance\n'\
-      '20/09/2022 || 100.00 || || 190.00\n'\
-      '20/09/2022 || 90.00 || || 90.00'
-    elsif @deposit == 50
-      return 'date || credit || debit || balance\n'\
-      '20/09/2022 || 50.00 || || 250.00\n'\
-      '20/09/2022 || 200.00 || || 200.00'
-    elsif @deposit != 0
-      currency = format('%.2f', @deposit)
-      return 'date || credit || debit || balance\n'\
-      "20/09/2022 || #{currency} || || #{currency}"
+    data = 'date || credit || debit || balance'
+    @transactions.each do |line|
+      data += "\n20/09/2022 || #{two_dec_pl(line[0])} || || #{two_dec_pl(line[1])}"
     end
-    'date || credit || debit || balance'
+    data
   end
 
   def deposit(amount)
     @balance += amount
-    @deposit = amount
+    @transactions.unshift([amount, @balance])
+  end
+
+  private
+
+  def two_dec_pl(num)
+    format('%.2f', num)
   end
 end
--- spec/single_deposit_spec.rb
+++ spec/single_deposit_spec.rb
@@ -2,39 +2,33 @@
 
 require('bank_account')
 RSpec.describe BankAccount do
-  before(:each) do
-    @my_account = BankAccount.new
+  before(:each) { @my_account = BankAccount.new }
+
+  it 'shows empty statement' do
+    expect(@my_account.statement).to eq('date || credit || debit || balance')
   end
 
-  context 'empty bank account' do
-    it 'shows empty statement' do
-      expect(@my_account.statement).to eq('date || credit || debit || balance')
-    end
+  it 'adds 200 and shows statement' do
+    @my_account.deposit(200)
+    statement = "date || credit || debit || balance\n20/09/2022 || 200.00 || || 200.00"
+    expect(@my_account.statement).to eq(statement)
   end
 
-  context 'single deposits' do
-    it 'adds 200 and shows statement' do
-      @my_account.deposit(200)
-      statement = 'date || credit || debit || balance\n20/09/2022 || 200.00 || || 200.00'
-      expect(@my_account.statement).to eq(statement)
-    end
+  it 'adds 100 and shows statement' do
+    @my_account.deposit(100)
+    statement = "date || credit || debit || balance\n20/09/2022 || 100.00 || || 100.00"
+    expect(@my_account.statement).to eq(statement)
+  end
 
-    it 'adds 100 and shows statement' do
-      @my_account.deposit(100)
-      statement = 'date || credit || debit || balance\n20/09/2022 || 100.00 || || 100.00'
-      expect(@my_account.statement).to eq(statement)
-    end
+  it 'adds 3.1415927 and shows statement' do
+    @my_account.deposit(3.1415927)
+    statement = "date || credit || debit || balance\n20/09/2022 || 3.14 || || 3.14"
+    expect(@my_account.statement).to eq(statement)
+  end
 
-    it 'adds 3.1415927 and shows statement' do
-      @my_account.deposit(3.1415927)
-      statement = 'date || credit || debit || balance\n20/09/2022 || 3.14 || || 3.14'
-      expect(@my_account.statement).to eq(statement)
-    end
-
-    it 'deposits 50 + 150' do
-      @my_account.deposit(50 + 150)
-      statement = 'date || credit || debit || balance\n20/09/2022 || 200.00 || || 200.00'
-      expect(@my_account.statement).to eq(statement)
-    end
+  it 'deposits 50 + 150' do
+    @my_account.deposit(50 + 150)
+    statement = "date || credit || debit || balance\n20/09/2022 || 200.00 || || 200.00"
+    expect(@my_account.statement).to eq(statement)
   end
 end
