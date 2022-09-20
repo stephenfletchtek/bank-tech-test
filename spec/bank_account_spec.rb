5d2925c9ae932dfda3e50cc370ede3654e61eb54
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-20 14:25:59 +0100

    refactor bank_account_spec.rb

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 require('bank_account')
-
 RSpec.describe BankAccount do
+  before(:each) do
+    @my_account = BankAccount.new
+  end
+
   context 'BankAccount class methods and behaviour' do
-    it 'creates and shows empty statement' do
-      my_account = BankAccount.new
-      result = my_account.statement
+    it 'shows empty statement' do
+      result = @my_account.statement
       statement = 'date || credit || debit || balance'
       expect(result).to eq(statement)
     end
 
-    it 'creates adds 200 and shows statement' do
-      my_account = BankAccount.new
-      my_account.deposit(200)
-      result = my_account.statement
+    it 'adds 200 and shows statement' do
+      @my_account.deposit(200)
+      result = @my_account.statement
       statement = 'date || credit || debit || balance\n'\
       '20/09/2022 || 200.00 || || 200.00'
       expect(result).to eq(statement)
