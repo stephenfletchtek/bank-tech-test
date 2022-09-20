aa260f8281c48e5f2c8e557af83e48026b5042ff
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-20 14:14:40 +0100

    ban_account_spec.rb cleaned up with rubocop

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 require('bank_account')
 
 RSpec.describe BankAccount do
-  context "BankAccount class methods and behaviour" do
-    it "creates and shows empty statement" do
+  context 'BankAccount class methods and behaviour' do
+    it 'creates and shows empty statement' do
       my_account = BankAccount.new
       result = my_account.statement
-      statement = "date || credit || debit || balance"
+      statement = 'date || credit || debit || balance'
       expect(result).to eq(statement)
     end
 
-    it "creates adds 200 and shows statement" do
+    it 'creates adds 200 and shows statement' do
       my_account = BankAccount.new
       my_account.deposit(200)
       result = my_account.statement
-      statement = "date || credit || debit || balance\n"\
-      "20/09/2022 || 200.00 || || 200.00"
+      statement = 'date || credit || debit || balance\n'\
+      '20/09/2022 || 200.00 || || 200.00'
       expect(result).to eq(statement)
     end
-
   end
 end
