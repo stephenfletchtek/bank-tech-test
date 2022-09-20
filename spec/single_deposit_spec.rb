b7e215b22bed3abcdd277faeb887f6e3d9353cc6
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-20 15:21:55 +0100

    added basic assumptions to single_deposit_spec

--- spec/single_deposit_spec.rb
+++ spec/single_deposit_spec.rb
@@ -24,5 +24,17 @@ RSpec.describe BankAccount do
       statement = 'date || credit || debit || balance\n20/09/2022 || 100.00 || || 100.00'
       expect(@my_account.statement).to eq(statement)
     end
+
+    it 'adds 3.1415927 and shows statement' do
+      @my_account.deposit(3.1415927)
+      statement = 'date || credit || debit || balance\n20/09/2022 || 3.14 || || 3.14'
+      expect(@my_account.statement).to eq(statement)
+    end
+
+    it 'deposits 50 + 150' do
+      @my_account.deposit(50 + 150)
+      statement = 'date || credit || debit || balance\n20/09/2022 || 200.00 || || 200.00'
+      expect(@my_account.statement).to eq(statement)
+    end
   end
 end
