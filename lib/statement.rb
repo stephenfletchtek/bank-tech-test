612c4c8e0588182914bd33d3499c6eece455f93e
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-21 14:45:40 +0100

    refactor statement.rb

--- lib/statement.rb
+++ lib/statement.rb
@@ -18,11 +18,17 @@ class Statement
     " || || #{two_dec_pl(-amount)} || "
   end
 
-  def formatted_transaction_list(bank_account)
+  def all_transactions_with_balance(bank_account)
     accumulator = bank_account.opening_balance
-    bank_account.all_transactions.map do |line|
-      accumulator += line[1]
-      line[0].strftime('%d/%m/%Y') + credit_or_debit(line[1]) + two_dec_pl(accumulator)
+    bank_account.all_transactions.map do |transaction| 
+      accumulator += transaction[1]
+      transaction << accumulator
+    end
+  end
+
+  def formatted_transaction_list(bank_account)
+    all_transactions_with_balance(bank_account).map do |line|
+      line[0].strftime('%d/%m/%Y') + credit_or_debit(line[1]) + two_dec_pl(line[2])
     end.reverse
   end
 end
